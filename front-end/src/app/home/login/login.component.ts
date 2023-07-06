c8ef333834160fe2cdc546afba68dc7309a36f92
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2023-07-06 13:28:33 -0300

    Implementacion Typescript
    Implementacion dle Typescript minimo e indispensable

--- front-end/src/app/home/login/login.component.ts
+++ front-end/src/app/home/login/login.component.ts
@@ -1,18 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/model/login';
+import { AuthService } from 'src/app/services/user/auth.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-  username: string = '';
-  password: string = '';
 
-  onSubmit() {
-    // Aquí puedes agregar la lógica para enviar los datos de inicio de sesión al servidor
-    // y realizar las validaciones necesarias
-    console.log('Usuario:', this.username);
-    console.log('Contraseña:', this.password);
+export class LoginComponent implements OnInit {
+  isLogged = false;
+  isLogginFail = false;
+  loginUser!: Login;
+  username!: string;
+  password! : string;
+  roles: string[] = [];
+  errMsj!: string;
+
+  constructor(private tokenService: TokenService, private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+      this.isLogginFail = false;
+      this.roles = this.tokenService.getAuthorities();
+    }
   }
+
+  onLogin(): void{
+    this.loginUser = new Login(this.username, this.password); 
+    this.authService.login(this.loginUser).subscribe(data =>{
+        this.isLogged = true;
+        this.isLogginFail = false;
+        this.tokenService.setToken(data.token);
+        this.tokenService.setUserName(data.username);
+        this.tokenService.setAuthorities(data.authorities);
+        this.roles = data.authorities;
+        this.router.navigate([''])
+      }, err =>{
+        this.isLogged = false;
+        this.isLogginFail = true;
+        this.errMsj = err.error.mensaje;
+        console.log(this.errMsj);
+        
+      })
+  }
+
 }
