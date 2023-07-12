109e974993c1c6f8485b06649d420bd3ea7a60ae
Abel3581 <marzoa3581@gmail.com>
2023-07-12 15:54:03 -0300

    act

--- front-end/src/app/home/product/products-category/products-category.component.ts
+++ front-end/src/app/home/product/products-category/products-category.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-products-category',
   templateUrl: './products-category.component.html',
   styleUrls: ['./products-category.component.css']
 })
-export class ProductsCategoryComponent {
+export class ProductsCategoryComponent implements OnInit{
+
+
+  products: Product[] = [];
+
+  constructor(private productService:ProductService,
+    private toastr: ToastrService,
+    private router: Router){}
+
+  ngOnInit(): void {
+
+  }
+
+  getProducts(categoryName: string){
+    this.productService.getProductsForCategory(categoryName).subscribe(
+      data => {
+        console.log(data);
+        this.products = data;
+      },
+      err =>{
+        console.error(err);
+      }
+    )
+  }
 
 }
