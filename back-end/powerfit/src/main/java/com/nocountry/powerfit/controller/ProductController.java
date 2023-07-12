8ad3d8a4a86d42246bedea8cf48f4336b7de3749
Santiago Daglio <santidaglio@gmail.com>
2023-07-12 01:46:28 -0300

    se agregan las modificaciones correspondientes para hacer funcionar busqueda y eliminación de productos

--- back-end/powerfit/src/main/java/com/nocountry/powerfit/controller/ProductController.java
+++ back-end/powerfit/src/main/java/com/nocountry/powerfit/controller/ProductController.java
@@ -4,7 +4,6 @@ import com.nocountry.powerfit.model.entity.Product;
 import com.nocountry.powerfit.model.exception.ResourceNotFoundException;
 import com.nocountry.powerfit.model.request.ProductRequest;
 import com.nocountry.powerfit.model.response.ProductResponse;
-import com.nocountry.powerfit.repository.IProductRepository;
 import com.nocountry.powerfit.service.abstraction.IProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -12,11 +11,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.util.List;
 @RequiredArgsConstructor
 @RestController
@@ -28,13 +25,31 @@ public class ProductController {
     @Autowired
     private IProductService iProductService;
 
-    @ApiOperation(value = "Registro de un producto", notes = "Retorna producto creado")
+
     @PostMapping("/add")
+    @ApiOperation(value = "Registro de un producto", notes = "Retorna producto creado")
     public ResponseEntity<ProductResponse> uploadFiles(
             @RequestParam(value="postimages", required = false) List<MultipartFile> postImage ,
             @RequestPart(value ="product", required = true) ProductRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(iProductService.add(postImage, request));
 
+    }
+
+    //metodo básico provisorio, agrega producto sin imagen.
+    @PostMapping("/addproduct")
+    @ApiOperation(value = "Agrega producto", notes = "Retorna Http 201 Created")
+    public ResponseEntity<Product> addProductWithoutUserAuth(@RequestBody Product product){
+        iProductService.save(product);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("/name/{name}")
+    @ApiOperation(value = "Busca producto por nombre", notes = "Retorna producto por nombre")
+    public ResponseEntity<List<ProductResponse>> findByName (@PathVariable String name) throws ResourceNotFoundException {
+        List<ProductResponse> response = iProductService.findByName(name);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @GetMapping("/all")
@@ -51,8 +66,16 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
+    @GetMapping("/category/{name}")
+    @ApiOperation(value = "Busca por categoría", notes = "Retorna lista de productos por categoría")
+    public ResponseEntity<List<ProductResponse>> getProductsForCategory (@PathVariable String name) throws ResourceNotFoundException {
+        List<ProductResponse> response = iProductService.getProductsForCategory(name);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+
     @DeleteMapping("/{id}")
-    @ApiOperation(value = "Busca por el id para eliminar registro ", notes = "Retorna http 204, no content")
+    @ApiOperation(value = "Borra producto por id", notes = "Retorna http 204, no content")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         iProductService.delete(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
