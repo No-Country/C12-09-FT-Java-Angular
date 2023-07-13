fa0622e02b48698dacf202899e5f2116dc3da277
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2023-07-13 02:43:48 -0300

    Responsive

--- front-end/src/app/home/navbar/navbar.component.ts
+++ front-end/src/app/home/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { TokenService } from 'src/app/services/token.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -13,11 +14,32 @@ export class NavbarComponent {
   constructor(private router:Router, private tokenService: TokenService) { }
 
   ngOnInit(): void {
-    if(this.tokenService.getToken()){
-      this.isLogged=true;
-    }else{
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
       this.isLogged = false;
     }
+
+    $(document).ready(() => {
+      $("#sidebarCollapse").on("click", () => {
+        $("#sidebar").addClass("active");
+      });
+
+      $("#sidebarCollapseX").on("click", () => {
+        $("#sidebar").removeClass("active");
+      });
+
+      $("#sidebarCollapse").on("click", () => {
+        if ($("#sidebar").hasClass("active")) {
+          $(".overlay").addClass("visible");
+          console.log("it's working!");
+        }
+      });
+
+      $("#sidebarCollapseX").on("click", () => {
+        $(".overlay").removeClass("visible");
+      });
+    });
   }
 
   onLogOut():void{
@@ -29,6 +51,7 @@ export class NavbarComponent {
     this.router.navigate(['/login'])
   }
 
+  
   
 }
 
