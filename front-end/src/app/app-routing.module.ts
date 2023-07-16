740f89f499ad3968a3da52c01e881beaf63e1438
mafer23 <mafer18199420@gmail.com>
2023-07-15 21:18:12 -0500

    cambios

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -4,17 +4,19 @@ import { HomeComponent } from './home/home.component'; // Asegúrate de importar
 import { RegisterComponent } from './home/register/register.component';
 import { LoginComponent } from './home/login/login.component';
 import { ProfileComponent } from './home/profile/profile.component';
+import { NosotrosComponent } from './home/nosotros/nosotros.component';
 
-// const routes: Routes = [
-//   { path: '', component: HomeComponent }, // Asegúrate de que la ruta para HomeComponent esté configurada correctamente
-//   { path: 'login', component: LoginComponent},
-//   { path: 'perfil', component: ProfileComponent},
-//   { path: 'register', component: RegisterComponent}
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Asegúrate de que la ruta para HomeComponent esté configurada correctamente
+  { path: 'login', component: LoginComponent},
+  { path: 'perfil', component: ProfileComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'nosotros', component: NosotrosComponent}
 
-// ];
+];
 
 @NgModule({
-  // imports: [RouterModule.forRoot(routes)],
-  // exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
