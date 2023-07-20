40b780546cd89475b96bdfe30493bbcbc98a8d85
Santiago Daglio <santidaglio@gmail.com>
2023-07-20 01:42:52 -0300

    modifico CartMapper

--- back-end/powerfit/src/main/java/com/nocountry/powerfit/model/mapper/CartMapper.java
+++ back-end/powerfit/src/main/java/com/nocountry/powerfit/model/mapper/CartMapper.java
@@ -1,16 +1,36 @@
 package com.nocountry.powerfit.model.mapper;
 
 import com.nocountry.powerfit.model.entity.Cart;
+import com.nocountry.powerfit.model.entity.Product;
 import com.nocountry.powerfit.model.entity.User;
 import com.nocountry.powerfit.model.request.CartRequest;
+import com.nocountry.powerfit.model.request.ProductRequest;
+import com.nocountry.powerfit.model.response.CartResponse;
+import com.nocountry.powerfit.model.response.ProductResponse;
+import org.springframework.stereotype.Component;
 
+import java.util.stream.Collectors;
+
+@Component
 public class CartMapper {
-    public Cart dtoToCart(CartRequest cartRequest){
+
+    public static CartResponse entityToDto(Cart cart) {
+        return CartResponse.builder()
+                .id(cart.getId())
+                .user(cart.getUser())
+                .products(cart.getProducts())
+                .amount(cart.getAmount())
+                .quantity(cart.getQuantity())
+                .build();
+    }
+
+    public static Cart dtoToEntity(CartRequest cartRequest) {
         return Cart.builder()
                 .id(cartRequest.getId())
                 .user(cartRequest.getUser())
-                .total(cartRequest.getTotal())
                 .products(cartRequest.getProducts())
+                .amount(cartRequest.getAmount())
+                .quantity(cartRequest.getQuantity())
                 .build();
     }
 }
